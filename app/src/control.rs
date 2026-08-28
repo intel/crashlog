eae992f5f37c1b2d1b034227f4494047af41859e
Rivas Paz, Jose L <jose.l.rivas.paz@intel.com>
2026-08-28 08:29:14 +0200

    app: enable control commands to trigger on all sources by default
    This patch enables the CLI to execute a control command on all the
    discoverable endpoints when no parameter was indicated with "-s"
    
    There is another change in the extraction flow as well, where the
    extracted data from PMT devices will be done only for the sys class PMT
    devices and not from the BDF devices.
    
    Signed-off-by: Rivas Paz, Jose L <jose.l.rivas.paz@intel.com>

--- app/src/control.rs
+++ app/src/control.rs
@@ -2,33 +2,66 @@
 // SPDX-License-Identifier: MIT
 
 use intel_crashlog::prelude::*;
+use intel_crashlog::source::Capability;
 
 pub fn rearm(sources: Vec<CrashLogSource>) -> Result<(), Error> {
-    control_command(sources, CrashLogSource::rearm)
+    run_control_command(sources, Capability::Rearm, CrashLogSource::rearm)
 }
 
 pub fn trigger(sources: Vec<CrashLogSource>) -> Result<(), Error> {
-    control_command(sources, CrashLogSource::trigger)
+    run_control_command(sources, Capability::Trigger, CrashLogSource::trigger)
 }
 
 pub fn clear(sources: Vec<CrashLogSource>) -> Result<(), Error> {
-    control_command(sources, CrashLogSource::clear)
+    run_control_command(sources, Capability::Clear, CrashLogSource::clear)
 }
 
 pub fn enable(sources: Vec<CrashLogSource>) -> Result<(), Error> {
-    control_command(sources, CrashLogSource::enable)
+    run_control_command(sources, Capability::EnableDisable, CrashLogSource::enable)
 }
 
 pub fn disable(sources: Vec<CrashLogSource>) -> Result<(), Error> {
-    control_command(sources, CrashLogSource::disable)
+    run_control_command(sources, Capability::EnableDisable, CrashLogSource::disable)
 }
 
-fn control_command<F>(sources: Vec<CrashLogSource>, control: F) -> Result<(), Error>
+fn run_control_command<F>(
+    sources: Vec<CrashLogSource>,
+    capability: Capability,
+    control: F,
+) -> Result<(), Error>
 where
     F: Fn(&CrashLogSource) -> Result<(), Error>,
 {
-    for source in sources {
-        control(&source)?;
+    let mut cmd_success = false;
+    let mut first_error = None;
+
+    let control_sources = if sources.is_empty() {
+        CrashLogSource::discover_distinct()
+            .into_iter()
+            .filter(|src| src.capabilities().contains(&capability))
+            .collect()
+    } else {
+        sources
+    };
+
+    if control_sources.is_empty() {
+        return Err(Error::NoCrashLogSourceFound);
+    }
+
+    for source in control_sources {
+        match control(&source) {
+            Ok(()) => cmd_success = true,
+            Err(err) => {
+                log::warn!("Error while running {capability} command on {source}: {err}");
+                if first_error.is_none() {
+                    first_error = Some(err);
+                }
+            }
+        }
+    }
+
+    if !cmd_success {
+        return Err(first_error.unwrap_or(Error::InternalError));
     }
 
     Ok(())
